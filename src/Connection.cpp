e2a563c9865d6fc2804cce37b66f2efcca052cea
Callum Knight <calknight@hotmail.com>
2021-11-13 12:32:00 +0000

    Began creating connection class.

---
+++ src/Connection.cpp
@@ -0,0 +1,40 @@
+#include "Connection.h"
+
+#define COLOUR_WHITE 255
+
+#define DEFAULT_COLOUR COLOUR_WHITE
+
+//--------------------------------------------------------------
+Connection::Connection(){
+
+    colour = DEFAULT_COLOUR;
+}
+
+//--------------------------------------------------------------
+Connection::~Connection(){
+
+}
+
+//--------------------------------------------------------------
+void Connection::setup(){
+
+}
+
+//--------------------------------------------------------------
+void Connection::update(){
+
+}
+
+//--------------------------------------------------------------
+void Connection::draw(float xStart, float yStart, float xEnd, float yEnd){
+
+    xPosStart = xStart;
+    yPosStart = yStart;
+    xPosEnd = xEnd;
+    yPosEnd = yEnd;
+
+    ofSetColor(colour);
+    ofPolyline();
+
+
+}
---
+++ src/Connection.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Node.h"
+
+class Connection{
+
+	public:
+		Connection();
+		~Connection();
+		void setup();
+		void update();
+		void draw(float xStart, float yStart, float xEnd, float yEnd);
+
+	private:
+		float xPosStart;
+		float yPosStart;
+		float xPosEnd;
+		float yPosEnd;
+};
