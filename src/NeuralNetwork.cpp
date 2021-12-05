4bf828c3e13969b0a945f17a7910bd170d572eb5
Callum Knight <calknight@hotmail.com>
2021-12-05 19:14:15 +0000

    Data generation does work so long as a release build is ran, or debug build is ran from the terminal - as opposed to VSCode. If you try run debug build from VSCode something happens to stdout which prevents data being written to files and prevents cout from working. There appears to be a problem with the neural network algorithm as forward prop and back prop produces nan values. It might have something to do with how the data is written to the test files or may be an issue in the algorithm itself.

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -12,12 +12,20 @@ void genData(std::string filename)
     std::ofstream file1(filename + "-in");
     std::ofstream file2(filename + "-out");
 
-    for(uint r = 0; r < 1000; r++)
+    if(file1 && file2)
     {
-        Scalar x = rand() / Scalar(RAND_MAX);
-        Scalar y = rand() / Scalar(RAND_MAX);
-        file1 << x << "," << y << std::endl;
-        file2 << 2 * x + 10 + y << std::endl;
+        std::cout << "Files created successfully." << std::endl;
+        for(uint r = 0; r < 1000; r++)
+        {
+            Scalar x = rand() / Scalar(RAND_MAX);
+            Scalar y = rand() / Scalar(RAND_MAX);
+            file1 << x << "," << y << std::endl;
+            file2 << 2 * x + 10 + y << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Error creating files." << std::endl;
     }
     
     file1.close();
